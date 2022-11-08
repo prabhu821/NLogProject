5243a2ceee53dc16b5da388883c7d8d39cd60cfe
rishiraj007 <rishiraj@mounttalent.com>
2022-11-08 09:31:42 +0530

    [Prabhu] Add: NLog Project Added

---
+++ NLogProject/AddNumbers.cs
@@ -0,0 +1,28 @@
+﻿using NLog.Fluent;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLogProject
+{
+    internal class AddNumbers
+    {
+        private readonly NLog nLog = new NLog();
+        public int Sum(int firstNumber, int secondNumber)
+        {
+            if (firstNumber == 0 || secondNumber == 0)
+            {
+                nLog.LogDebug("Debug Successfull : Sum()");
+                nLog.LogError("Expecting null values");
+                nLog.LogWarn("secondNumber or secondNumber should not be 0");
+            }
+            int result=firstNumber+secondNumber;
+            nLog.LogDebug("Debug Successfull : Sum()");
+            nLog.LogInfo("Sum Method passed, Result" +result);
+            return result;
+       
+        }
+    }
+}
